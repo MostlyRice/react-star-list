0e82f26af4240731fae0599c580ec28870a5ed39
Zygy93 <amazinzygy@gmail.com>
2018-03-15 14:25:06 -0500

    curried

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -5,13 +5,38 @@ import './App.css';
 class App extends Component {
 constructor(props) {
   super(props);
+
   this.state = { 
+    newStar: {
+      Name: '',
+      Diameter: '',
+    },
+    
     starlist:[ {Name:'Gacrux', Diameter:'117 Million Kilometers'}, {Name:'Fomalhaut',Diameter:'2 Miles'}, 
     {Name:'Hadar', Diameter:'13 Million Kilometers'}]
-    }
+  },
+    this.handleClick = this.handleClick.bind(this);
   };
 
+  handleChangeFor= (propertyName) => (event) => {
+    this.setState({
+      newStar: {
+        ...this.state.newStar,
+        [propertyName]: event.target.value,
+      }
+    });
+  }
 
+  handleClick(){
+    console.log("clicked");
+    this.setState({
+      newStar: {
+        Name: '',
+        Diameter: '',
+      },
+      starlist: [...this.state.starlist, this.state.newStar],
+    })
+  }
 
   render() {
     return (
@@ -20,8 +45,14 @@ constructor(props) {
         <img src={logo} className="App-logo" alt="logo" />
         <h1 className="App-title">Welcome to React</h1>
       </header>
+      <input value={this.state.newStar.Name} onChange={this.handleChangeFor('Name')} />
+      <input value={this.state.newStar.Diameter} onChange={this.handleChangeFor('Diameter')} />
+      <button onClick={this.handleClick}> Add a new Star!</button>
       <p>
-      {this.state.starlist.map( (star,i) => <p key={star}>  {star.Name}, {star.Diameter} </p>)}
+        This star {this.state.newStar.Name} and it'a diameter is {this.state.newStar.Diameter}
+      </p>
+      <p>
+      {this.state.starlist.map( (star) => <p key={star}>The star is {star.Name} and it's diameter is {star.Diameter} </p>)}
       </p>
       </div>
     );
